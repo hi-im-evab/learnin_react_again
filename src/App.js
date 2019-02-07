926962bda6ecb28ffa500821bf0ed24d010efa12
hi-im-evab <navesregge@gmail.com>
2019-02-06 20:07:12 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Anal Contusions
           </a>
         </header>
       </div>
