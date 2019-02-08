5a1da50066f4df6939e5a11e64d00ec27cfad232
hi-im-evab <navesregge@gmail.com>
2019-02-07 19:53:31 -0600

    screw tictactoe. did Axios get for pokemon info and displayed info in DOM

--- src/index.js
+++ src/index.js
@@ -1,12 +1,182 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class YourComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null
+        }
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    componentDidMount() {
+        const axios = require('axios');
+
+        var pokeboy;
+
+        const that = this
+        const setState = this.setState.bind(this);
+
+        console.log('pre async');
+
+        axios.get("https://intern-pokedex.myriadapps.com/api/v1/pokemon?name=Pikachu")
+            .then(function (response) {
+                // handle success
+                that.setState();
+                console.log('async');
+
+                console.log(response);
+
+                pokeboy = response.data.data[0];
+
+                //that.setState({pokeboy: pokeboy});
+
+                console.log(pokeboy.id);
+                console.log(pokeboy.image);
+                console.log(pokeboy.name);
+                console.log(pokeboy.types[0]);
+
+                var array = [pokeboy.id, pokeboy.image, pokeboy.name, pokeboy.types[0]];
+
+                setState({ data: array });
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+
+    }
+
+    render() {
+        return (<div>
+            POKEMON INFO
+            {this.state.data === null ?
+                <div>Loading</div>
+                :
+                <div>
+                    {this.state.data[0]} <br></br>
+                    <img src={this.state.data[1]}></img> <br></br>
+                    {this.state.data[2]} <br></br>
+                    {this.state.data[3]} <br></br>
+
+                </div>
+            }
+        </div>);
+    }
+}
+
+
+
+ReactDOM.render(
+    // <Game />,
+    <YourComponent />,
+    document.getElementById('root')
+);
+
+
+
+// class Square extends React.Component {
+//     render() {
+//         return (
+//             <button className="square">
+//                 {/* TODO */}
+//             </button>
+//         );
+//     }
+// }
+
+// class Board extends React.Component {
+
+
+//     renderSquare(i) {
+//         return <Square />;
+//     }
+
+//     render() {
+//         const status = 'Next player: X';
+
+//         return (
+//             <div>
+//                 <div className="status">{status}</div>
+//                 <div className="board-row">
+//                     {this.renderSquare(0)}
+//                     {this.renderSquare(1)}
+//                     {this.renderSquare(2)}
+//                 </div>
+//                 <div className="board-row">
+//                     {this.renderSquare(3)}
+//                     {this.renderSquare(4)}
+//                     {this.renderSquare(5)}
+//                 </div>
+//                 <div className="board-row">
+//                     {this.renderSquare(6)}
+//                     {this.renderSquare(7)}
+//                     {this.renderSquare(8)}
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+// class Game extends React.Component {
+//     shitDicks() {
+//         const axios = require('axios');
+
+//         var pokeboy = 'stupid';
+
+//         const that = this;
+
+//         axios.get("https://intern-pokedex.myriadapps.com/api/v1/pokemon?name=Pikachu")
+//             .then(function (response) {
+//                 that.setState();
+
+
+//                 console.log('1');
+
+//                 // handle success
+//                 console.log(response);
+
+//                 pokeboy = response.data.data[0];
+
+//                 //that.setState({pokeboy: pokeboy});
+
+//                 console.log(pokeboy.id);
+//                 console.log(pokeboy.image);
+//                 console.log(pokeboy.name);
+//                 console.log(pokeboy.types[0]);
+//             })
+//             .catch(function (error) {
+//                 // handle error
+//                 console.log(error);
+//             })
+//             .then(function () {
+//                 // always executed
+//             });
+
+
+//         console.log('2');
+//     }
+
+//     render() {
+//         return (
+//             <div className="game">
+//                 <div className="game-board">
+//                     <Board />
+//                 </div>
+//                 <div className="game-info">
+//                     <div>{/* status */}</div>
+//                     <ol>{/* TODO */}</ol>
+//                 </div>
+
+//                 <div>
+//                     {this.state && this.state.data &&
+//                         <div>{'This will just render after the return of the async call'}</div>
+//                     }
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+// ========================================
